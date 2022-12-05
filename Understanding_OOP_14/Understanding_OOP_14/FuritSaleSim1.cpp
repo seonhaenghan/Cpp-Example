bff0454a2681e9432a5a812e24d9fcbd35e4a9c2
SeonHaeng Han <zaqzse55@gmail.com>
2022-12-05 12:41:53 +0900

    221205 Rev.A Understanding_OOP_14 Code add

---
+++ Understanding_OOP_14/Understanding_OOP_14/FuritSaleSim1.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+class FuritSeller
+{
+private:
+	int APPLE_PRICE;
+	int numOfApples;
+	int myMoney;
+public:
+	void InitMebers(int price, int num, int money)
+	{
+		APPLE_PRICE = price;
+		numOfApples = num;
+		myMoney = money;
+	}
+
+	int SaleApples(int money)
+	{
+		int num = money / APPLE_PRICE;
+		numOfApples -= num;
+		myMoney += money;
+		return num;
+	}
+
+	void ShowSalesResult()
+	{
+		cout << "남은 사과: " << numOfApples << endl;
+		cout << "판매 수익: " << myMoney << endl << endl;
+	}
+};
+
+class FuritBuyer
+{
+	int myMoney;
+	int numOfApples;
+public:
+	void InitMembers(int money)
+	{
+		myMoney = money;
+		numOfApples = 0;
+	}
+	void BuyApples(FuritSeller& seller, int money)
+	{
+		numOfApples += seller.SaleApples(money);
+		myMoney -= money;
+	}
+	
+	void ShowBuyResult()
+	{
+		cout << "현재 잔액: " << myMoney << endl;
+		cout << "사과 개수: " << numOfApples << endl << endl; 
+	}
+};
+
+int main(void)
+{
+	FuritSeller seller;
+	seller.InitMebers(1000, 20, 0);
+	FuritBuyer buyer;
+	buyer.InitMembers(5000);
+	buyer.BuyApples(seller, 2000);		// 과일 구매
+
+	cout << "과일 판매자의 현황" << endl;
+	seller.ShowSalesResult();
+	cout << "과일 구매자의 현황" << endl;
+	buyer.ShowBuyResult();
+
+	return 0;
+}
